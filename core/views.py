d4209d49fc7b68faded646912973508207669ab3
Tushar-PC\Tushar <tamyworld@gmail.com>
2017-05-21 12:04:13 +0530

    add the view to render Test data from all the databases

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from core.models import TestData
+
+def getTestData(request):
+    """returns the test data from all the databases"""
+    raise NotImplementedError
