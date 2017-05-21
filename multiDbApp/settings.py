782eff0af60ef8b6f1322ce5c6933b3d96f4ad30
Tushar-PC\Tushar <tamyworld@gmail.com>
2017-05-21 12:02:28 +0530

    register the middleware

--- multiDbApp/settings.py
+++ multiDbApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.WhichDatabaseIsTOUseMIddleware'
 ]
 
 ROOT_URLCONF = 'multiDbApp.urls'
