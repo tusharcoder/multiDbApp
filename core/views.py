f22cde954d56870c3232d47ec2143c695e818240
Tushar-PC\Tushar <tamyworld@gmail.com>
2017-05-21 12:24:11 +0530

    implement to show all data from each multiple database

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
+# @Date:   2017-05-20T19:44:50+05:30
+# @Last modified time: 2017-05-21T12:21:10+05:30
+
+
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +10,8 @@ from core.models import TestData
 
 def getTestData(request):
     """returns the test data from all the databases"""
-    raise NotImplementedError
+    databases=('testdb1','testdb2','testdb3','testdb4','testdb5')
+    data=[]
+    for database in databases:
+        data.append(TestData.objects.using(database).values_list('desc',flat=True))
+    return render(request,'index.html',{"title":"TestData",'data':data})
